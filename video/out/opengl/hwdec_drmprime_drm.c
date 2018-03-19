72e006b5a92e7f65af9673dc5e35fe9f6d3c4d6f
LongChair <longchair@hotmail.com>
2018-03-19 07:01:58 +0100

    drmprime interop : Add HDR playback support
    This Commit adds support for HDR playback thru drm atomic.
    The atomic properties that are used in that commit didn't make it to mainline yet but are implemented by a bunch of vendors. using them if they don't exist will not cause any issues.
    
    Here are a few references for incoming implementations :
    DRI : https://patchwork.kernel.org/patch/9756449/
    Intel : https://lists.freedesktop.org/archives/dri-devel/2017-May/143145.html
            https://patchwork.kernel.org/patch/9756423/
            https://lists.freedesktop.org/archives/intel-gfx/2017-April/125451.html
    
    This work had been tested on RockChip devices (Rock64) which uses the same implementation.

--- video/out/opengl/hwdec_drmprime_drm.c
+++ video/out/opengl/hwdec_drmprime_drm.c
@@ -39,6 +39,98 @@
 
 extern const struct m_sub_options drm_conf;
 
+// The HDR structs and enumerations didn't make it to mainline yet.
+// those can be removed once support has landed
+enum mp_supported_eotf_type {
+        MP_TRADITIONAL_GAMMA_SDR = 0,
+        MP_TRADITIONAL_GAMMA_HDR,
+        MP_SMPTE_ST2084,
+        MP_HLG,
+        MP_FUTURE_EOTF
+};
+
+enum mp_v4l2_colorspace {
+    /*
+     * Default colorspace, i.e. let the driver figure it out.
+     * Can only be used with video capture.
+     */
+    V4L2_COLORSPACE_DEFAULT       = 0,
+
+    /* SMPTE 170M: used for broadcast NTSC/PAL SDTV */
+    V4L2_COLORSPACE_SMPTE170M     = 1,
+
+    /* Obsolete pre-1998 SMPTE 240M HDTV standard, superseded by Rec 709 */
+    V4L2_COLORSPACE_SMPTE240M     = 2,
+
+    /* Rec.709: used for HDTV */
+    V4L2_COLORSPACE_REC709        = 3,
+
+    /*
+     * Deprecated, do not use. No driver will ever return this. This was
+     * based on a misunderstanding of the bt878 datasheet.
+     */
+    V4L2_COLORSPACE_BT878         = 4,
+
+    /*
+     * NTSC 1953 colorspace. This only makes sense when dealing with
+     * really, really old NTSC recordings. Superseded by SMPTE 170M.
+     */
+    V4L2_COLORSPACE_470_SYSTEM_M  = 5,
+
+    /*
+     * EBU Tech 3213 PAL/SECAM colorspace. This only makes sense when
+     * dealing with really old PAL/SECAM recordings. Superseded by
+     * SMPTE 170M.
+     */
+    V4L2_COLORSPACE_470_SYSTEM_BG = 6,
+
+    /*
+     * Effectively shorthand for V4L2_COLORSPACE_SRGB, V4L2_YCBCR_ENC_601
+     * and V4L2_QUANTIZATION_FULL_RANGE. To be used for (Motion-)JPEG.
+     */
+    V4L2_COLORSPACE_JPEG          = 7,
+
+    /* For RGB colorspaces such as produces by most webcams. */
+    V4L2_COLORSPACE_SRGB          = 8,
+
+    /* AdobeRGB colorspace */
+    V4L2_COLORSPACE_ADOBERGB      = 9,
+
+    /* BT.2020 colorspace, used for UHDTV. */
+    V4L2_COLORSPACE_BT2020        = 10,
+
+    /* Raw colorspace: for RAW unprocessed images */
+    V4L2_COLORSPACE_RAW           = 11,
+
+    /* DCI-P3 colorspace, used by cinema projectors */
+    V4L2_COLORSPACE_DCI_P3        = 12,
+};
+
+enum mp_drm_hdmi_output_type {
+    MP_DRM_HDMI_OUTPUT_DEFAULT_RGB, /* default RGB */
+    MP_DRM_HDMI_OUTPUT_YCBCR444, /* YCBCR 444 */
+    MP_DRM_HDMI_OUTPUT_YCBCR422, /* YCBCR 422 */
+    MP_DRM_HDMI_OUTPUT_YCBCR420, /* YCBCR 420 */
+    MP_DRM_HDMI_OUTPUT_YCBCR_HQ, /* Highest subsampled YUV */
+    MP_DRM_HDMI_OUTPUT_YCBCR_LQ, /* Lowest subsampled YUV */
+    MP_DRM_HDMI_OUTPUT_INVALID, /* Guess what ? */
+};
+
+/* HDR Metadata */
+struct mp_hdr_static_metadata {
+        uint16_t eotf;
+        uint16_t type;
+        uint16_t display_primaries_x[3];
+        uint16_t display_primaries_y[3];
+        uint16_t white_point_x;
+        uint16_t white_point_y;
+        uint16_t max_mastering_display_luminance;
+        uint16_t min_mastering_display_luminance;
+        uint16_t max_fall;
+        uint16_t max_cll;
+        uint16_t min_cll;
+};
+
 struct drm_frame {
     struct drm_prime_framebuffer fb;
     struct mp_image *image; // associated mpv image
@@ -51,6 +143,13 @@ struct priv {
 
     struct drm_atomic_context *ctx;
     struct drm_frame current_frame, last_frame, old_frame;
+
+    // Media HDR metadata
+    struct mp_hdr_static_metadata *hdr_metadata;
+    int hdr_blob_id;
+
+    // Panel HDR metadata
+    struct mp_hdr_static_metadata panel_metadata;
 
     struct mp_rect src, dst;
 
@@ -127,6 +226,52 @@ static void disable_video_plane(struct ra_hwdec *hw)
     }
 }
 
+static struct mp_hdr_static_metadata *get_hdr_metadata(struct mp_image *mpi)
+{
+    struct mp_hdr_static_metadata *hdr;
+
+    if (!mpi)
+        return NULL;
+
+    hdr = talloc_zero(NULL, struct mp_hdr_static_metadata);
+
+    switch (mpi->params.color.gamma) {
+    case MP_CSP_TRC_PQ:
+        hdr->eotf = MP_SMPTE_ST2084;
+        break;
+    case MP_CSP_TRC_HLG:
+        hdr->eotf = MP_HLG;
+        break;
+    default:
+        hdr->eotf = MP_TRADITIONAL_GAMMA_SDR;
+        break;
+    }
+
+    struct mp_csp_primaries prims = mp_get_csp_primaries(mpi->params.color.primaries);
+    hdr->display_primaries_x[0] = (prims.red.x * 50000);
+    hdr->display_primaries_x[1] = (prims.green.x * 50000);
+    hdr->display_primaries_x[2] = (prims.blue.x * 50000);
+    hdr->display_primaries_y[0] = (prims.red.y * 50000);
+    hdr->display_primaries_y[1] = (prims.green.y * 50000);
+    hdr->display_primaries_y[2] = (prims.blue.y * 50000);
+    hdr->white_point_x = (prims.white.x * 50000);
+    hdr->white_point_y = (prims.white.y * 50000);
+
+    return hdr;
+}
+
+static enum mp_v4l2_colorspace mp_get_hdr_colorspace(enum mp_csp_trc trc)
+{
+    switch(trc) {
+        case MP_CSP_TRC_PQ:
+        case MP_CSP_TRC_HLG:
+            return V4L2_COLORSPACE_BT2020;
+
+        default:
+            return V4L2_COLORSPACE_DEFAULT;
+    }
+}
+
 static int overlay_frame(struct ra_hwdec *hw, struct mp_image *hw_image,
                          struct mp_rect *src, struct mp_rect *dst, bool newframe)
 {
@@ -136,6 +281,13 @@ static int overlay_frame(struct ra_hwdec *hw, struct mp_image *hw_image,
     drmModeAtomicReq *request = NULL;
     struct drm_frame next_frame = {0};
     int ret;
+
+    // grab drm interop info
+    struct mpv_opengl_cb_drm_params *drmparams =
+            gl ? (struct mpv_opengl_cb_drm_params *)
+            mpgl_get_native_display(gl, "opengl-cb-drm-params") : NULL;
+    if (drmparams)
+        request = (drmModeAtomicReq *)drmparams->atomic_request;
 
     if (hw_image) {
 
@@ -150,13 +302,6 @@ static int overlay_frame(struct ra_hwdec *hw, struct mp_image *hw_image,
             p->dst = *dst;
         }
         p->src = *src;
-
-        // grab drm interop info
-        struct mpv_opengl_cb_drm_params *drmparams =
-                gl ? (struct mpv_opengl_cb_drm_params *)
-                mpgl_get_native_display(gl, "opengl-cb-drm-params") : NULL;
-        if (drmparams)
-            request = (drmModeAtomicReq *)drmparams->atomic_request;
 
         next_frame.image = hw_image;
         desc = (AVDRMFrameDescriptor *)hw_image->planes[0];
@@ -184,6 +329,35 @@ static int overlay_frame(struct ra_hwdec *hw, struct mp_image *hw_image,
                 drm_object_set_property(request,  p->ctx->video_plane, "CRTC_W",  dstw);
                 drm_object_set_property(request,  p->ctx->video_plane, "CRTC_H",  dsth);
                 drm_object_set_property(request,  p->ctx->video_plane, "ZPOS",    0);
+
+                if (!p->hdr_metadata) {
+                    p->hdr_metadata = get_hdr_metadata(hw_image);
+                    drmModeCreatePropertyBlob(drmparams->fd,  p->hdr_metadata, sizeof(* p->hdr_metadata), &p->hdr_blob_id);
+                    mp_verbose(p->log, "Video detected as %s\n", p->hdr_metadata->eotf ? "HDR" : "SDR");
+                }
+
+                if (p->hdr_metadata) {
+
+                    // send hdr to panel only if it supports hdr
+                    if (p->panel_metadata.eotf != 0) {
+                        drm_object_set_property(request,  p->ctx->connector, "HDR_SOURCE_METADATA", p->hdr_blob_id);
+                        drm_object_set_property(request,  p->ctx->video_plane, "EOTF", p->hdr_metadata->eotf);
+                    }
+                    else {
+                        drm_object_set_property(request,  p->ctx->video_plane, "EOTF",
+                                                p->hdr_metadata->eotf ? MP_TRADITIONAL_GAMMA_HDR : MP_TRADITIONAL_GAMMA_SDR);
+                    }
+
+                    drm_object_set_property(request,  p->ctx->video_plane, "COLOR_SPACE",
+                                            mp_get_hdr_colorspace(hw_image->params.color.space));
+
+                    drm_object_set_property(request,  p->ctx->connector, "HDMI_OUTPUT_FORMAT", MP_DRM_HDMI_OUTPUT_YCBCR_HQ);
+                }
+                else {
+                    drm_object_set_property(request,  p->ctx->video_plane, "EOTF", MP_TRADITIONAL_GAMMA_SDR);
+                    drm_object_set_property(request,  p->ctx->video_plane, "COLOR_SPACE", V4L2_COLORSPACE_DEFAULT);
+                }
+
             } else {
                 ret = drmModeSetPlane(p->ctx->fd, p->ctx->video_plane->id, p->ctx->crtc->id, next_frame.fb.fb_id, 0,
                                       MP_ALIGN_DOWN(p->dst.x0, 2), MP_ALIGN_DOWN(p->dst.y0, 2), dstw, dsth,
@@ -197,6 +371,19 @@ static int overlay_frame(struct ra_hwdec *hw, struct mp_image *hw_image,
         }
     } else {
 
+        drm_object_set_property(request,  p->ctx->video_plane, "EOTF", MP_TRADITIONAL_GAMMA_SDR);
+        drm_object_set_property(request,  p->ctx->video_plane, "COLOR_SPACE", V4L2_COLORSPACE_DEFAULT);
+        drm_object_set_property(request,  p->ctx->connector, "HDMI_OUTPUT_FORMAT", MP_DRM_HDMI_OUTPUT_DEFAULT_RGB);
+        drm_object_set_property(request,  p->ctx->connector, "HDR_SOURCE_METADATA", 0);
+
+        // End of playback, free playback specific data
+        if (p->hdr_metadata) {
+            talloc_free(p->hdr_metadata);
+            p->hdr_metadata = NULL;
+
+            drmModeDestroyPropertyBlob(p->ctx->fd, p->hdr_blob_id);
+            p->hdr_blob_id = 0;
+        }
         disable_video_plane(hw);
     }
 
@@ -275,6 +462,16 @@ static int init(struct ra_hwdec *hw)
     }
 
     disable_video_plane(hw);
+
+    drmModePropertyBlobPtr panel_metadata_prop;
+    panel_metadata_prop = drm_object_get_property_blob(p->ctx->connector, "HDR_PANEL_METADATA");
+    if (panel_metadata_prop) {
+        memcpy(&p->panel_metadata, panel_metadata_prop->data, panel_metadata_prop->length);
+        if (p->panel_metadata.eotf)
+            MP_VERBOSE(hw, "Panel supports HDR\n");
+        drmFree(panel_metadata_prop);
+    }
+
     return 0;
 
 err:
